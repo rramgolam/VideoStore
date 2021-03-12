1e828cfa0fe5cc05d0fe8329944f100d5aece901
Rishi Ramgolam <rishi.ramgolam@madetech.com>
2021-03-12 14:29:45 +0000

    First refactor. All tests passing.

--- VideoStore/VideoStore.Test/RentalTest.cs
+++ VideoStore/VideoStore.Test/RentalTest.cs
@@ -12,55 +12,48 @@ namespace VideoStore.Test
         }
 
         [Test]
-        public void whenIAdd2DaysToRental_thenThePriceShouldBe2Pounds()
+        public void whenIHave2DayRental_thenThePriceShouldBe2Pounds()
         {
-            RentalCalculator rentalCalculator = new RentalCalculator();
             float expectedPrice = 2.0f;
-            rentalCalculator.Days = 2;
-            rentalCalculator.CalculatePrice();
+
+            int days = 2;
+            RentalCalculator rentalCalculator = new RentalCalculator(days);
 
             Assert.AreEqual(expectedPrice, rentalCalculator.Price);
         }
 
         [Test]
-        public void whenIAdd5DaysToMyRental_thenThePriceShouldBe6pounds50pence()
+        public void whenIHaveA5DayRental_thenThePriceShouldBe6pounds50pence()
         {
-            RentalCalculator rentalCalculator = new RentalCalculator();
             float expectedPrice = 6.5f;
-            rentalCalculator.Days = 5;
-            rentalCalculator.CalculatePrice();
+
+            int days = 5;
+            RentalCalculator rentalCalculator = new RentalCalculator(days);
 
             Assert.AreEqual(expectedPrice, rentalCalculator.Price);
         }
 
         [Test]
-        public void whenIAddNDaysToRental_thenMyFrequentRentalPointsEarntWillBe1()
+        public void whenIHaveAnNDayRental_thenMyFrequentRentalPointsEarntWillBe1()
         {
-            RentalCalculator rentalCalculator = new RentalCalculator();
             int expectedPoints = 1;
-            Random randomDay = new Random();
-            rentalCalculator.Days = randomDay.Next(1,10);
-            rentalCalculator.CalculateFrequenceRentalPoints();
 
+            int days = new Random().Next(1, 10);
+            RentalCalculator rentalCalculator = new RentalCalculator(days);
+            
             Assert.AreEqual(expectedPoints, rentalCalculator.FrequentRentalPointsEarnt);
         }
 
         [Test]
         public void whenIHave2Rentals_thenMyFrequentRentalPointsEarntWillBe2()
         {
-            RentalCalculator firstRentalCalculator = new RentalCalculator();
-            RentalCalculator secondRentalCalculator = new RentalCalculator();
-
             int expectedPoints = 2;
 
-            Random randomDay = new Random();
-            firstRentalCalculator.Days = randomDay.Next(1, 10);
-            secondRentalCalculator.Days = randomDay.Next(1, 10);
+            RentalCalculator firstRentalCalculator = new RentalCalculator(new Random().Next(1, 10));
+            RentalCalculator secondRentalCalculator = new RentalCalculator(new Random().Next(1, 10));
 
-            firstRentalCalculator.CalculateFrequenceRentalPoints();
-            secondRentalCalculator.CalculateFrequenceRentalPoints();
-
-            Assert.AreEqual(expectedPoints, firstRentalCalculator.FrequentRentalPointsEarnt + firstRentalCalculator.FrequentRentalPointsEarnt);
+            Assert.AreEqual(expectedPoints, firstRentalCalculator.FrequentRentalPointsEarnt
+                + secondRentalCalculator.FrequentRentalPointsEarnt);
         }
     }
 }
--- VideoStore/VideoStore/Models/RentalCalculator.cs
+++ VideoStore/VideoStore/Models/RentalCalculator.cs
@@ -3,24 +3,34 @@ namespace VideoStore.Models
 {
     public class RentalCalculator
     {
-        public int Days { get; set; }
-        public float Price { get; private set; }
-        public int FrequentRentalPointsEarnt { get; private set; }
-
-        public void CalculatePrice()
+        public RentalCalculator(int numberOfDays)
         {
-            if (Days <= 2)
-            {
-                this.Price = 2;
-            } else
-            {
-                this.Price = 1.5f * (this.Days - 2) + 2.0f;
+            this.Days = numberOfDays;
+
+        }
+
+        public int Days { get; set; }
+
+        public float Price {
+            get {
+                return calculatePrice();
             }
         }
 
-        public void CalculateFrequenceRentalPoints()
-        {
-            this.FrequentRentalPointsEarnt = 1;
+        public int FrequentRentalPointsEarnt {
+            get { return 1; }
         }
+
+        private float calculatePrice()
+        {
+            if (Days <= 2)
+            {
+                return 2;
+            } else
+            {
+                return 1.5f * (this.Days - 2) + 2.0f;
+            }
+        }
+
     }
 }
