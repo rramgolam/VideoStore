00e7b3127589f964d5798e0edfe9638cab40b8f5
Rishi Ramgolam <rishi.ramgolam@madetech.com>
2021-03-12 15:01:12 +0000

    First new movie rental test failing.

--- VideoStore/VideoStore.Test/NewMovieRentalTest.cs
+++ VideoStore/VideoStore.Test/NewMovieRentalTest.cs
@@ -1,8 +1,19 @@
 ﻿using System;
+using NUnit.Framework;
+using VideoStore.Models;
+
 namespace VideoStore.Test
 {
     public class NewMovieRentalTest
     {
+
+        [Test]
+        public void whenIHaveAThreeDayRental_thenThePriceIs9Pounds() {
+            int days = 3;
+            RentalCalculator rentalCalculator = new RentalCalculator(days);
+            
+            Assert.AreEqual(9.0, rentalCalculator.Price);
+        }
 
     }
 }
